e9fe7f2cf05087f7d9f43b4e7198302968d186c3
christian <christiandavidphoto@gmail.com>
2018-05-29 11:10:08 -0400

    code comments

--- src/App.js
+++ src/App.js
@@ -12,18 +12,23 @@ class App extends Component {
   };
 
   render() {
+    // destructuring to maximize readability below
     const { recipes } = this.state;
     return (
       <div className="App container">
         <PanelGroup accordion>
+          {/* display all recipes in parent array */}
           {recipes.map((recipe, index) => (
             <Panel eventKey={index}>
               <Panel.Heading>
+                {/* display recipe name */}
                 <Panel.Title toggle>{recipe.recipeName}</Panel.Title>
               </Panel.Heading>
               <Panel.Body collapsible>
+                {/* map over ingrs array to display ingr list */}
                 <ul>{recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}</ul>
                 <ButtonToolbar>
+                  {/* delete or edit a recipe */}
                   <Button bsStyle="danger">Delete Recipe</Button>
                   <Button bsStyle="default">Edit Recipe</Button>
                 </ButtonToolbar>
@@ -32,6 +37,7 @@ class App extends Component {
           ))}
         </PanelGroup>
 
+        {/* add a recipe */}
         <Button bsStyle="primary">Add Recipe</Button>
       </div>
     );
