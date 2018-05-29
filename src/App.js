8ac72b65a46140f2042afedac1b5c94432968d22
christian <christiandavidphoto@gmail.com>
2018-05-29 11:03:30 -0400

    accordion comp deprecated, refactored to Panel family of comps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-import Accordion from 'react-bootstrap/lib/Accordion';
-import Panel from 'react-bootstrap/lib/Panel';
-import Button from 'react-bootstrap/lib/Button';
-import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
-import Modal from 'react-bootstrap/lib/Modal';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import FormControl from 'react-bootstrap/lib/FormControl';
+import { Panel, PanelGroup, Button, ButtonToolbar, Modal, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
@@ -21,14 +14,19 @@ class App extends Component {
   render() {
     const { recipes } = this.state;
     return (
-      <div className="App">
-        <Accordion>
+      <div className="App container">
+        <PanelGroup accordion>
           {recipes.map((recipe, index) => (
-            <Panel header={recipe.recipeName} eventKey={index} key={index}>
-              d
+            <Panel eventKey={index}>
+              <Panel.Heading>
+                <Panel.Title toggle>{recipe.recipeName}</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body collapsible>
+                <ul>{recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}</ul>
+              </Panel.Body>
             </Panel>
           ))}
-        </Accordion>
+        </PanelGroup>
       </div>
     );
   }
